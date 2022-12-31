85546c4d9f59abb484f12b0a1ba4d9586b5ea780
Saber Choukoukou <95322711+5483R@users.noreply.github.com>
2022-12-31 16:55:24 +0000

    updates

--- ex01/Main.cpp
+++ ex01/Main.cpp
@@ -49,6 +49,7 @@ Phonebook ft_add(Phonebook phone, int i)
 }
 void ft_search()
 {
+  int i = 0;
   std::string index;
   std::string fname;
   std::string sname;
@@ -56,11 +57,27 @@ void ft_search()
 
   
     std::cout << "------------------------------------------------------------------"<<std::endl;
-    while()
+    std::cout << "|Index     |First name|Last name |Nickname  |" <<std::endl;
+    while(i < this->Contact)
     {
-
+      std::cout << "|";
+      std::cout << std::setw(10) << i << "|";
+      if(this->Contact[i].get_fname().length() > 10)
+          std::cout << std::setw(10) << this->Contact[i].get_fname().substr(0, 9) + "." << "|";
+      else
+          std::cout << std::setw(10) << this->Contact[i].get_fname() >> "|";
+      if(this->Contact[i].get_sname().length() > 10)
+          std::cout << std::setw(10) << this->Contact[i].get_sname().substr(0, 9) + "." << "|";
+      else
+          std::cout << std::setw(10) << this->Contact[i].get_sname() >> "|";
+      if(this->Contact[i].get_surnom().length() > 10)
+          std::cout << std::setw(10) << this->Contact[i].get_surnom().substr(0, 9) + "." << "|";
+      else
+          std::cout << std::setw(10) << this->Contact[i].get_surnom() >> "|";
+      std::cout << std::endl;
+      std::cout << "------------------------------------------------------------------"<<std::endl;
+      i++;
     }
-    std::cout << "|" << "Index" << std::setw(10) << "First name" << std::setw(10) << std::setw(10) << "Last Name" << std::setw(10) << "Nickname"<<std::endl;
 }
 
 int main() {
