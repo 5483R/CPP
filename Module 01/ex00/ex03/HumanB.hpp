f01fc29653acad3a21338c82f9178695f46ea8dd
Saber Choukoukou <95322711+5483R@users.noreply.github.com>
2023-01-09 23:30:20 +0100

    Add files via upload

---
+++ Module 01/ex00/ex03/HumanB.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Weapon.hpp"
+
+class HumanB{
+    private:
+    std::string name;
+    Weapon *weapon;
+    public:
+    HumanB();
+    HumanB(std::string name);
+    void setWeapon(Weapon weapon);
+    void attack();
+};
